24a11217b9b656a25defed5fa96b5e6ab1fa867f
mustkov <dolzhenkov74@gmail.com>
2022-02-08 20:42:14 +0400

    Edited

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getRepository } from "./actions/takeCitiesDatas";
 import "./App.scss";
+import { setCurrentCity_Redux, setDoneCities } from "./reducers/reposReducer";
 
 function App() {
   const dispatch = useDispatch();
   const repos = useSelector((state) => state.repos);
+  const doneCities = useSelector((state) => state.repos.doneCities);
+  const doneCity = useSelector((state) => state.repos.city);
+
+  const [city, setCity] = useState('')
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -14,26 +19,50 @@ function App() {
     dispatch(getRepository());
   }, []);
 
+  const inputChange = (e) => {
+    setCurrentCity(e.target.value);
+  }
+
+  const buttonClick = () => {
+    dispatch(setCurrentCity_Redux(city));
+    arrayDoneCities()
+  }
+
+  const arrayDoneCities = () => {
+    if(!doneCities.includes(doneCity)) {
+      dispatch(setDoneCities(doneCity))
+    } else {
+      alert('Такой город уже был!')
+    }
+  }
+
+  const setCurrentCity = (value) => {
+    setCity(value)
+  }
+
   return (
     <div className="App">
       <header>
         <span>Игра в города</span>
       </header>
-        <div className="input-group mb-3 md">
+        <form className="input-group mb-3 md" onSubmit={handleSubmit}>
           <input
             type="text"
             className="form-control"
             aria-describedby="button-addon2"
+            onChange={(e) => inputChange(e)}
           />
           <button
             className="btn btn-outline-secondary"
-            type="button"
+            type="submit"
             id="button-addon2"
+            onClick={buttonClick}
           >
             Button
           </button>
-        </div>
+        </form>
         <p>Введённый Вами город: </p>
+        <h3>{city}</h3>
         {/* <p>{state.city}</p> */}
         <p>Город компьютера: </p>
         {/* {state.randomConfirmCity ? <p>{state.randomConfirmCity.name}</p> : null} */}
--- src/reducers/reposReducer.js
+++ src/reducers/reposReducer.js
@@ -1,16 +1,36 @@
 const SET_REPOS = "SET_REPOS";
 const SET_IS_FETCHING = "SET_IS_FETCHING";
 const SET_FETCH_ERROR = "SET_FETCH_ERROR";
+const SET_CITY = "SET_CITY";
+const SET_DONE_CITIES = "SET_DONE_CITIES";
+const SET_LAST_LETTER = "SET_LAST_LETTER";
 
 const defaultState = {
-  value: "Привет",
+  city: "",
   startBaseCities: [],
   isFetching: true,
   isFetchError: false,
+  doneCities: [],
+  lastLetter: "",
 };
 
 export default function reposReducer(state = defaultState, action) {
   switch (action.type) {
+    case SET_DONE_CITIES:
+      return {
+        ...state,
+        doneCities: [...state.doneCities, action.payload],
+      };
+    case SET_LAST_LETTER:
+      return {
+        ...state,
+        lastLetter: action.payload,
+      };
+    case SET_CITY:
+      return {
+        ...state,
+        city: action.payload,
+      };
     case SET_IS_FETCHING:
       return {
         ...state,
@@ -35,6 +55,10 @@ export const setRepos = (repos) => ({
   type: SET_REPOS,
   payload: repos,
 });
+export const setCurrentCity_Redux = (city) => ({
+  type: SET_CITY,
+  payload: city,
+});
 export const setIsFetching = (bool) => ({
   type: SET_IS_FETCHING,
   payload: bool,
@@ -42,4 +66,12 @@ export const setIsFetching = (bool) => ({
 export const setFetchError = (bool) => ({
   type: SET_FETCH_ERROR,
   payload: bool,
+});
+export const setDoneCities = (city) => ({
+  type: SET_DONE_CITIES,
+  payload: city,
+});
+export const setLastLetter = (symbol) => ({
+  type: SET_LAST_LETTER,
+  payload: symbol,
 });
